c0c9234e36ef0a4a56f3dc3ac6196a912cb52d6e
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-16 22:01:28 +0000

    changed auth register user to accept username

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,6 +25,24 @@
                             </div>
                         </div>
 
+
+                        <!--username -->
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">UserName</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
+
+                                @if ($errors->has('username'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('username') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
+
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
 
